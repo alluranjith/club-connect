1e369b1631ddc76a2c05f50dcee7ed572a833cfa
ALLU RANJITH KUMAR <aranjitharanjith0@gmail.com>
2026-09-09 20:36:15 +0530

    Delete member directory

--- member/MemberNotifications.jsx
+++
@@ -1,6 +0,0 @@
-import NotificationManager from '../../components/shared/NotificationManager';
-
-// Members (both accepted and non-club) can only view notifications, not post them
-const MemberNotifications = () => <NotificationManager canPost={false} />;
-
-export default MemberNotifications;
